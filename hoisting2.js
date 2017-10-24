8a4e2e61f19d3f3104b8b5002fefe8b33c01a6fe
hoangthau <thau.nguyen@orientsoftware.net>
2017-10-24 11:01:12 +0700

    Update hoisting2.js

--- hoisting2.js
+++ hoisting2.js
@@ -1,3 +1,10 @@
 console.log(hoist); // Output: undefined
 
 var hoist = 'The variable has been hoisted.';
+
+
+//the same with
+var hoist;
+
+console.log(hoist); // Output: undefined
+hoist = 'The variable has been hoisted.';
