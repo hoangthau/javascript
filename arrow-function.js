5e84a5900a0daebe951291ba3cb7919bcf9f29de
hoangthau <thau.nguyen@orientsoftware.net>
2017-10-28 16:57:29 +0700

    Create arrow-function.js

---
+++ arrow-function.js
@@ -0,0 +1,29 @@
+// example 1
+([param] [, param]) => {
+  statements
+}
+
+// example 2
+param => expression
+
+
+//ES5
+// example 1
+function ([param] [, param]) {
+  statements
+}
+
+// example 2
+function (param) {
+  return expression
+}
+
+// ES5
+function doSomething() {
+  //...
+}
+
+// ES6
+var doSomething = () => {
+  //...
+}
